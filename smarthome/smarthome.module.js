6484b9ea014e1a5d1337d632ee9dce6733996131
LauncherBridge <virilseraph@gmx.net>
2026-03-16 00:42:55 +0100

    Update smarthome.module.js

--- smarthome/smarthome.module.js
+++ smarthome/smarthome.module.js
@@ -43,7 +43,7 @@ window.SmartHomeModule = {
         }
 
         // Load template from smarthome.html
-        fetch("smarthome.html")
+        fetch("index.html")
             .then(res => res.text())
             .then(html => {
                 container.innerHTML = html;
