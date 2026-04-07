798f2a2aebf8d0ef0e99ad788de540c20843dac1
LauncherBridge <virilseraph@gmx.net>
2026-04-08 00:46:03 +0200

    Update smarthome.editor.js

--- smarthome/smarthome.editor.js
+++ smarthome/smarthome.editor.js
@@ -654,12 +654,12 @@ onDown(e) {
     }
 
 
-// PAN-Kandidat merken (aber noch NICHT starten)
-if (hit.type === "empty" || hit.type === "wall") {
-    this.isPanCandidate = true;
-    this.panStartX = mouseX;
-    this.panStartY = mouseY;
-}
+    // PAN-Kandidat merken (aber noch NICHT starten)
+    if (this.isClosed && (hit.type === "empty" || hit.type === "wall")) {
+        this.isPanCandidate = true;
+        this.panStartX = mouseX;
+        this.panStartY = mouseY;
+    }
 },
 
 
