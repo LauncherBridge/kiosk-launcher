405afb4e65efaaa30f7b31a05b95a6ba870cf82b
LauncherBridge <virilseraph@gmx.net>
2026-04-11 07:19:41 +0200

    Update door icon handling in smarthome.editor.js
    Replaced doorSVG with doorIcon for better image handling.

--- smarthome/smarthome.editor.js
+++ smarthome/smarthome.editor.js
@@ -1,5 +1,6 @@
-const doorSVG =
-"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath fill='white' d='M6 2h12v20H6V2zm7 10a1 1 0 11-2 0 1 1 0 012 0z'/%3E%3C/svg%3E";
+const doorIcon = new Image();
+doorIcon.src =
+"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Crect x='2' y='2' width='20' height='20' fill='black'/%3E%3C/svg%3E";
 
 const doorIcon = new Image();
 const svgImg = new Image();
