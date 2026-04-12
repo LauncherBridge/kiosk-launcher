c9aa046b729fe5c9682b92d7dfbf44bc26efd358
LauncherBridge <virilseraph@gmx.net>
2026-04-12 22:13:02 +0200

    Fix boolean assignment for door state

--- smarthome/smarthome.editor.js
+++ smarthome/smarthome.editor.js
@@ -1595,7 +1595,7 @@ case "zimmertuer": {
         // ------------------------------------------------------------
 case "haustuer": {
 
-    d.isOpen = false; // nur zum Testen
+    d.isOpen = True; // nur zum Testen
 
     // Drehpunkt bestimmen (Scharnier)
     const hx = (d.hinge === "start") ? x1 : x2;
