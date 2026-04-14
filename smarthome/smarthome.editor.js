204b6f51217be5d95e76ebe76ea831a74108b454
LauncherBridge <virilseraph@gmx.net>
2026-04-15 01:46:31 +0200

    Change Gartentor state to open for testing

--- smarthome/smarthome.editor.js
+++ smarthome/smarthome.editor.js
@@ -2263,7 +2263,7 @@ case "garagentor": {
         // ------------------------------------------------------------
 case "gartentor": {
 
-    d.isOpen = false; // nur zum Testen
+    d.isOpen = true; // nur zum Testen
 
     // 0. Drehpunkt bestimmen (Scharnier)
     const hx = (d.hinge === "start") ? x1 : x2;
