0054b2eb2694a7a9d40dc7492c26dec3a4686779
LauncherBridge <virilseraph@gmx.net>
2026-04-20 03:14:10 +0200

    Enable d.isOpen for dachluke case
    Set d.isOpen to true for testing purposes.

--- smarthome/smarthome.editor.js
+++ smarthome/smarthome.editor.js
@@ -2744,6 +2744,7 @@ case "durchgang": {
 // ⭐ Dachluke
 // ------------------------------------------------------------     
 case "dachluke": {
+    d.isOpen = true; // nur zum Testen
 
     const r = d.width / 2;
 
